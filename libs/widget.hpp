9b771164ad70e101de9be565853f9397c1b632bf
nizarll <forsubsofmy@gmail.com>
2026-01-05 21:48:05 +0000

    Added bunch of components to widget refactor

--- libs/widget.hpp
+++ libs/widget.hpp
@@ -1,14 +1,28 @@
 #pragma once
+
 #include <concepts>
+#include <QObject>
 #include <QBoxLayout>
+#include <QEvent>
 #include <QPushButton>
 #include <QWidget>
 #include <QString>
-#include <qboxlayout.h>
-#include <qwidget.h>
+#include <QBoxLayout>
+#include <QLabel>
+#include <QLineEdit>
+#include <QSpinBox>
+#include <QWidget>
+
+#include <qcombobox.h>
+#include <qlist.h>
+#include <qnamespace.h>
+#include <qspinbox.h>
+#include <ranges>
 #include <string>
 #include <tuple>
-#include <type_traits>
+#include <variant>
+
+#include "utils.hpp"
 
 template <typename T>
 concept CreatableWidget = requires (T widget, QWidget* parent, QBoxLayout* layout) {
@@ -23,6 +37,7 @@ auto add_widgets_to(QWidget* parent, QBoxLayout* layout, std::tuple<Widgets...> 
       members.create_widget(parent, layout);
     }()), ...);
   }, widgets);
+  layout->addStretch();
 }
 
 template <typename T>
@@ -34,10 +49,9 @@ public:
     self.m_alignment = alignment;
     return self;
   }
-
   virtual auto create_widget(QWidget* parent, QBoxLayout* layout) -> QWidget* = 0;
 
-private:
+protected:
   Qt::Alignment m_alignment{Qt::AlignCenter};
   uint m_stretch{};
 };
@@ -64,19 +78,21 @@ struct Layout : public Widget<Layout<Widgets...>> {
     } else {
       layout = new QHBoxLayout();
     }
-    if (m_spacing) layout->setSpacing(m_spacing);
+    m_layout_ptr = layout;
+    if (m_spacing) layout->setSpacing(m_spacing.value());
     if (m_stretched) layout->addStretch();
 
     ::add_widgets_to(parent, layout, m_widgets);
-    parent_layout->addLayout(layout);
+    if (parent_layout)
+      parent_layout->addLayout(layout);
     return parent;
   }
 
   auto fitTo(QWidget* parent)
   {
     auto layout = new QHBoxLayout();
-    auto widget = create_widget(parent, layout);
-    parent->setLayout(layout);
+    auto widget = create_widget(parent, nullptr);
+    parent->setLayout(m_layout_ptr);
   }
 
   auto withSpacing(this auto&& self, uint spacing)
@@ -93,9 +109,10 @@ struct Layout : public Widget<Layout<Widgets...>> {
 
 private:
   std::tuple<Widgets...> m_widgets;
-  uint m_spacing{};
+  std::optional<bool> m_stretched{};
+  std::optional<uint> m_spacing{};
+  QBoxLayout* m_layout_ptr;
   LayoutDirection m_direction;
-  bool m_stretched{false};
 };
 
 template <typename... Widgets>
@@ -128,3 +145,111 @@ private:
   std::string m_text;
 };
 
+struct Text : public Widget<Text> {
+  Text() = default;
+  Text(std::string&& text) : m_text(std::move(text)) {}
+  Text(const std::string& text) : m_text(text) {}
+
+  auto create_widget(QWidget* parent, QBoxLayout* layout) -> QWidget*
+  {
+    auto* widget = new QLabel(QString::fromStdString(m_text), parent);
+    layout->addWidget(widget);
+    return widget;
+  }
+private:
+  std::string m_text;
+};
+
+struct Input: public Widget<Input> {
+  enum class ValueType {
+    U64,
+    F64,
+    String
+  };
+  Input(
+    ValueType type = ValueType::U64,
+    std::optional<std::string> placeholder = {},
+    std::optional<std::variant<f64, u64, std::string>> default_value = {}
+  ): m_placeholder(placeholder),
+    m_type(type),
+    m_default_value(default_value)
+  {}
+
+
+  auto valueChanged(this auto&& self, auto listener)
+  {
+    self.m_value_changed = listener;
+    return self;
+  }
+  auto create_widget(QWidget* parent, QBoxLayout* layout) -> QWidget*
+  {
+    QWidget* widget;
+    QSpinBox* uint_widget;
+    QDoubleSpinBox* float_widget;
+    QLineEdit* string_widget;
+    switch (m_type) {
+    case ValueType::U64:
+      uint_widget = new QSpinBox(parent);
+      uint_widget->setValue(std::get<f64>(m_default_value.value_or(u64{})));
+      widget = uint_widget;
+      ::QObject::connect(uint_widget, &QSpinBox::textChanged, [this](QString val){
+        m_value = std::stoul(val.toStdString());
+        m_value_changed.value_or([](decltype(m_value)){ })(m_value);
+      });
+      break;
+    case ValueType::F64:
+      float_widget = new QDoubleSpinBox(parent);
+      float_widget->setValue(std::get<f64>(m_default_value.value_or(f64{})));
+      widget = float_widget;
+      ::QObject::connect(float_widget, &QDoubleSpinBox::textChanged, [this](QString val){
+        m_value = std::stod(val.toStdString());
+        m_value_changed.value_or([](decltype(m_value)){ })(m_value);
+      });
+      break;
+    case ValueType::String:
+      string_widget = new QLineEdit(parent);
+      string_widget->setPlaceholderText(QString::fromStdString(m_placeholder.value_or("")));
+      widget = string_widget;
+      ::QObject::connect(string_widget, &QLineEdit::textChanged, [this](QString val){
+        m_value = std::stod(val.toStdString());
+        m_value_changed.value_or([](decltype(m_value)){ })(m_value);
+      });
+      break;
+    }
+    layout->addWidget(widget);
+    return widget;
+  }
+private:
+  std::variant<f64, u64, std::string> m_value;
+  std::optional<std::string> m_placeholder;
+  std::optional<std::variant<f64, u64, std::string>> m_default_value;
+  std::optional<std::function<void(std::variant<f64, u64, std::string>)>> m_value_changed;
+  ValueType m_type{ValueType::U64};
+};
+
+struct ComboBox : public Widget<Button> {
+  ComboBox() = default;
+  ComboBox(
+    std::optional<std::string> base_value = {},
+    std::optional<std::vector<std::string>> options = {}
+  ) : m_base_value(base_value), m_options(options) {}
+
+
+  auto create_widget(QWidget* parent, QBoxLayout* layout) -> QWidget*
+  {
+    auto* widget = new QComboBox(parent);
+    widget->setPlaceholderText(QString::fromStdString(m_base_value.value_or("")));
+
+    auto items = m_options.value_or({})                           |
+                              std::views::transform(&QString::fromStdString) |
+                              std::ranges::to<QStringList>();
+    widget->addItems(items);
+    layout->addWidget(widget);
+    return widget;
+  }
+
+private:
+  std::optional<std::string> m_base_value;
+  std::optional<std::vector<std::string>> m_options;
+  std::optional<std::function<void(std::string)>> m_value_changed;
+};
